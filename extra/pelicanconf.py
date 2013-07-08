df6b2f984ab3c48cd24f6ef38887beb777889147
bussiere <bussiere@gmail.com>
2013-07-08 09:38:23 +0200

    rajout

--- extra/pelicanconf.py
+++ extra/pelicanconf.py
@@ -18,4 +18,24 @@ SOCIAL = (('Bussiere', 'http://bussiere.github.com/'),)
 
 DEFAULT_PAGINATION = 10
 
+
 THEME = "../pelican-themes/bootstrap2"
+PLUGIN_PATH = "../pelican-plugins/"
+PLUGINS = ['sitemap',]
+FEED_MAX_ITEMS = 15
+FEED_ATOM = 'feeds/all.atom.xml'
+FEED_RSS = 'feeds/all.rss.xml'
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'daily',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
