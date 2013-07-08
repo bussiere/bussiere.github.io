d9819b8305c6567c381c3735dc7e3aa4c6126690
bussiere <bussiere@gmail.com>
2013-07-08 15:36:47 +0200

    rajout link

--- extra/pelicanconf.py
+++ extra/pelicanconf.py
@@ -14,7 +14,7 @@ LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           ('Python.org', 'http://python.org'),
           ('Jinja2', 'http://jinja.pocoo.org'),)
 # Social widget
-SOCIAL = (('Bussiere', 'http://bussiere.github.com/'),)
+SOCIAL = (('Bussiere', 'https://github.com/bussiere'),)
 
 DEFAULT_PAGINATION = 10
 
